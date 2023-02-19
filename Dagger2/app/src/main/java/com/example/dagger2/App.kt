774cd9dc399e896c582212d22d007256157b99d9
Yashnarbek Melikov <94248004+yashnarbek-melikov@users.noreply.github.com>
2023-02-19 19:22:33 +0500

    dagger 2

---
+++ Dagger2/app/src/main/java/com/example/dagger2/App.kt
@@ -0,0 +1,19 @@
+package com.example.dagger2
+
+import android.app.Application
+import com.example.dagger2.di.component.AppComponent
+import com.example.dagger2.di.component.DaggerAppComponent
+import com.example.dagger2.di.module.DatabaseModule
+
+class App : Application() {
+
+    companion object {
+        lateinit var appComponent: AppComponent
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent =
+            DaggerAppComponent.builder().databaseModule(DatabaseModule(applicationContext)).build()
+    }
+}
